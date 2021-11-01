83610c49ba99001c54ce1889891044cad129e972
POut53 <89731216+POut53@users.noreply.github.com>
2021-11-01 15:11:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var noseX=0;
+var simp="";
+var noseY=0;
+var diffrence=0;
+var leftWristX=0;
+var rigthWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500)
+    canvas=createCanvas(500,500);
+    canvas.position(500,150);
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',getPose)
+}
+function press(){
+    simp=document.getElementById("text").value;
+}
+function draw(){
+    background('yellow');
+    text(simp,26,100);
+}
+function modelloaded(){
+    console.log("PoseNet is Initiaizied");
+}
+function getPose(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log(noseX,noseY);
+    }
+}
+
